cd363dec0fc4e7942ceab733e367013044349e90
Guedes <albertoguedes4@gmail.com>
2023-09-26 09:17:29 +0100

    Java for beginners

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -1,5 +1,18 @@
 public class HundredthPrime {
     public static void main(String[] args) {
-        /* Make a code that displays the 100th prime number */
+        int count = 1;
+        int num = 1;
+
+        while (count < 100) {
+            num++;
+            for (int divisor = num - 1; divisor > 1; divisor--) {
+                if (num % divisor == 0) {
+                    break;
+                } else if (divisor == 2) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(num);
     }
 }

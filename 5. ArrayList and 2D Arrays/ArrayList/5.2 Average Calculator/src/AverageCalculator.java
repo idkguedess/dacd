4913bc32450d1baf13b9b5c9dfe44c0b2f765abd
Guedes <albertoguedes4@gmail.com>
2023-09-29 13:18:46 +0100

    Java for beginners

--- 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
+++ 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 
 public class AverageCalculator {
     public static double getAverageOf(ArrayList<Integer> numbers) {
-        /* TODO */
+        double suma = 0;
+
+        for (int number : numbers) {
+            suma += number;
+        }
+        return suma / numbers.size();
     }
 }
